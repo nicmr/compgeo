9d037230eea67b6f22b2430caa49ea7d55ca2171
Nicolas Mohr <Nico.Mohr@gmx.net>
2019-11-05 10:44:09 +0100

    basic cH_bound implementation

--- src/Convex.elm
+++ src/Convex.elm
@@ -2,8 +2,8 @@ module Convex exposing (..)
 
 import Math.Vector2 exposing (Vec2)
 
-convex : List Vec2 -> List Vec2
-convex list =
+convexHull : List Vec2 -> List Vec2
+convexHull list =
     let
         ordered = List.sortWith compare_x list
         reverse = List.reverse ordered
@@ -12,12 +12,50 @@ convex list =
         [(Math.Vector2.vec2 1 2)]
 
 
+-- upper : List Vec2 -> List Vec2
+-- upper list =
+
+
+
+cH_bound_wrapper : List Vec2 -> List Vec2
+cH_bound_wrapper list =
+    case list of
+        [] -> []
+        [x] -> [x]
+        (a::xsa) -> case xsa of
+            [] -> [a]
+            [x] -> [a, x]
+            (b::xsb) -> case xsb of
+                [] -> [a, b]
+                [x] -> [a, b, x]
+                (c::xsc) -> cH_bound a b c [] xsc
+
+
+-- Recursively calculates a single boundary of a convex hull (upper or lower)
+cH_bound : Vec2 -> Vec2 -> Vec2 -> List Vec2 -> List Vec2 -> List Vec2
+cH_bound a b c acc rest =
+    case rightTurn a b c of
+        True ->
+            case rest of
+                [] -> List.reverse acc -- used as stack so far, now reverse for desired order
+                [x] -> cH_bound b c x (a :: acc) []
+                (x::xs) -> cH_bound b c x (a :: acc) xs
+        False ->
+            case acc of
+                [] -> []
+                [x] -> cH_bound a b x [] rest
+                (x::xs) -> cH_bound a b x xs rest
+
+
+
+
+
 compare_x : Vec2 -> Vec2 -> Order
 compare_x a b =
     compare (Math.Vector2.getX a) (Math.Vector2.getX b)
 
-right_turn : Vec2 -> Vec2 -> Vec2 -> Bool
-right_turn a b c =
+rightTurn : Vec2 -> Vec2 -> Vec2 -> Bool
+rightTurn a b c =
     let
         slope = Math.Vector2.sub b a
         scale = (Math.Vector2.sub c a |> Math.Vector2.getX ) / Math.Vector2.getX(slope)
