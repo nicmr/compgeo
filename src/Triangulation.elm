264b5c0b5c54d660339a2050143bc4fff3fd8f5c
Nicolas Mohr <Nico.Mohr@gmx.net>
2019-11-10 19:02:36 +0100

    complete overhaul of Triangulation module

--- src/Triangulation.elm
+++ src/Triangulation.elm
@@ -4,61 +4,98 @@ module Triangulation exposing (..)
 import Math.Vector2 exposing (Vec2, getX, getY)
 import Array exposing (Array)
 import Set exposing (Set)
+import VectorSet exposing (VectorSet)
 
--- Polygons are modelled as List Vec2 that stores an ordered list of corner vertices.
+-- Polygons in this module are implemented as a List Vec2 that stores an ordered list of corner vertices.
+-- Vertices are implemented as Vec2
+-- Line segments are implemented as (Vec2, Vec2), where the Vec2 represent start and end point of the line segment
 
--- Determines if the passed polygon is y-monotone
-isYMonotone : List Vec2 -> Bool
-isYMonotone polygon =
+
+-- Triangulates a polygon and returns a list of the edges of the corresponding triangles
+-- Only works with y-monotone polygons so far, will return `Nothing` for non-monotone polygons.
+triangulate : List Vec2 -> Maybe (List (Vec2, Vec2))
+triangulate polygon =
+    let
+        (left, right) = splitAtMax polygon
+    in 
+        if isYMonotone left && isYMonotone right then
+            Just <| diagonal_wrapper (VectorSet.fromList left) (VectorSet.fromList right) polygon
+        else
+            Nothing
+
+-- Splits the two passed lists at the vertex with the highest y-Value
+splitAtMax : List Vec2 -> (List Vec2, List Vec2)
+splitAtMax polygon =
     let
         with_index = List.indexedMap (\i val -> (i,val)) polygon 
         polygon_array = Array.fromList polygon
     in
         case with_index of
-            -- Polygons with 0 and 1 corners are always y-monotone
-            [] -> True
-            [x] -> True
+            [] -> ([], [])
+            [x] -> ([Tuple.second x], [Tuple.second x])
             (x::xs) ->
                 -- get max y-Index
                 List.foldl (\(index_a, a) (index_b, b) -> if getY a >= getY b then (index_a, a) else (index_b, b)) x xs
                 |> Tuple.first
-                -- split into two at max
-                |> (\index -> [Array.slice 0 index polygon_array, Array.slice index (Array.length polygon_array) polygon_array])
-                |> List.map (\arr -> Array.toList arr)
-                |> List.map (\sublist -> List.map (\vec2 -> getY vec2) sublist)
-                |> List.map (
-                    \sublist ->
-                        case sublist of
-                            [] -> True
-                            [_] -> True
-                            -- (x::xs) -> List.foldl monotonePredicate AccNothing xs
-                            (y::ys) ->
-                                case List.foldl monotonePredicate (Just y) ys of
-                                    Just _ -> True
-                                    Nothing -> False
-                    )
-                |> List.foldl (\z acc -> (&&) z acc) True
+                -- split into two lists at max
+                |> (\index ->   ( Array.toList <| Array.slice 0 index polygon_array
+                                , Array.toList <| Array.slice index (Array.length polygon_array
+                                )
+                                polygon_array))
+
+-- Determines if the passed polygon is y-monotone
+isYMonotone : List Vec2 -> Bool
+isYMonotone sequence =
+    let
+         -- we only need the y-coordinates
+        y_only = List.map (\vec -> getY vec) sequence
+    in  
+        case y_only of
+            -- Line Sequences with 0 and 1 vertices are always y-monotone
+            [] -> True
+            [_] -> True
+            (y::ys) ->
+                case List.foldl monotonePredicate (Just y) ys of
+                    Just _ -> True
+                    Nothing -> False
 
 
-monotonePredicate : number -> Maybe number -> Maybe number
+monotonePredicate : Float -> Maybe Float -> Maybe Float
 monotonePredicate x acc =
     case acc of
         Nothing -> Nothing
         Just a -> if x<a then Just x else Nothing
 
--- diagonal : Vec2 -> Vec2 -> Vec2 -> List Vec2 -> List Vec2 -> List (Vec2, Vec2) -> Set Vec2 -> Set Vec2 -> List Vec2
--- diagonal p q r rest stack acc left right =
---     let
---         side =  if Set.member p left then
---                     left
---                 else right
---     in
---         -- TODO: check for edges outside the polygon, use stack
---         case rest of
---             [] -> (p, q) :: acc
---             [last] -> diagonal r p last  [] [] ((p, r) :: acc) left right
---             (next::newRest) ->
---                 if Set.member r side then
---                     diagonal r p next  newRest [] ((p, r) :: acc) left right
---                 else
---                     diagonal r q next newRest [] ((q, r) :: acc) left right
+
+-- wrapper around tail recursive diagonal function
+diagonal_wrapper : VectorSet -> VectorSet -> List Vec2 -> List(Vec2, Vec2)
+diagonal_wrapper left right polygon =
+    case polygon of
+        [] -> []
+        [a] -> []
+        (a::xsa) -> case xsa of
+            [] -> []
+            [b] -> []
+            (b::xsb) -> case xsb of
+                [] -> []
+                [c] -> diagonal a b c [] [] [] left right
+                (c::xsc) -> diagonal a b c xsc [] [] left right
+
+
+diagonal : Vec2 -> Vec2 -> Vec2 -> List Vec2 -> List Vec2 -> List (Vec2, Vec2) -> VectorSet -> VectorSet -> List (Vec2,Vec2)
+diagonal p q r rest stack acc left right =
+    let
+        side =  if VectorSet.member p left then
+                    left
+                else right
+    in
+        -- TODO: check for edges outside the polygon ( => use stack)
+        case rest of
+            [] -> (p, q) :: acc
+            [last] -> diagonal r p last  [] [] ((p, r) :: acc) left right
+            (next::newRest) ->
+                if VectorSet.member r side then
+                    diagonal r p next  newRest [] ((p, r) :: acc) left right
+                else
+                    diagonal r q next newRest [] ((q, r) :: acc) left right
+
