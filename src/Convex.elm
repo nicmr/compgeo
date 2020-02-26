925cfa9025291fa24340bb8060ff49fd76f38c1e
Nicolas Mohr <Nico.Mohr@gmx.net>
2020-02-26 19:19:39 +0100

    massively simplified andrew algorithm implementation

--- src/Convex.elm
+++ src/Convex.elm
@@ -4,99 +4,67 @@ module Convex exposing (convexHull)
 @docs convexHull
 -}
 
-import Math.Vector2 exposing (Vec2)
-
-
-type alias TurnPredicate = (Vec2 -> Vec2 -> Vec2 -> Bool)
-type Mode = Upper | Lower
-
+import Math.Vector2 exposing (Vec2, getY, getX)
 
 {-| Constructs a convex hull of vectors in the passed list.
 The returned list will be an ordered list of the points on the hull boundary, starting with the point of the lowest x Value.
 The last point will be identical to the first.
+Uses Andrew's algorithm with a complexity of O (n log n)
 -} 
 convexHull : List Vec2 -> List Vec2
-convexHull list =
+convexHull vertices =
     let
-        upper_bound = boundWrapper Upper list
-        lower_bound = boundWrapper Lower list
+        sortedByX = List.sortWith compare_x vertices
+        upper = constructChain sortedByX
+        lower = constructChain <| List.reverse sortedByX
     in
-        List.append upper_bound <| Maybe.withDefault [] <| List.tail lower_bound
+        List.append upper <| Maybe.withDefault [] <| List.tail lower
 
-
--- Wrapper around bound. Constructs and returns a vertice boundary from the passed list.
-boundWrapper: Mode -> List Vec2 -> List Vec2
-boundWrapper mode list =
-    let
-        increasing = List.sortWith compare_x list
-
-        (predicate, ordered) = case mode of
-            Upper -> (rightTurnUpper, increasing)
-            Lower -> (rightTurnLower, List.reverse increasing)
-    in  
-        case ordered of
-            [] -> []
-            [a] -> [a]
-            (a::xsa) -> case xsa of
-                [] -> [a]
-                [b] -> [a, b]
-                (b::xsb) -> case xsb of
-                    [] -> [a, b]
-                    [c] -> bound predicate a b c [] []
-                    (c::xsc) -> bound predicate a b c [] xsc
-
-
--- Recursively calculates a single boundary of a convex hull,
--- ensuring any given 3 consequitive points form a right turn in accordnace with the passed predicate.
-bound : TurnPredicate -> Vec2 -> Vec2 -> Vec2 -> List Vec2 -> List Vec2 -> List Vec2
-bound predicate a b c acc rest =
-    let
-        bound_pred = bound predicate
-    in 
-        case predicate a b c of
-            True ->
-                case rest of
-                    [] -> (List.reverse acc) ++ [a,b,c] -- used as stack so far, now reverse for desired order
-                    [x] -> bound_pred b c x (a :: acc) []
-                    (x::xs) -> bound_pred b c x (a :: acc) xs
-            False ->
-                case acc of
-                    [] -> case rest of
-                        [] -> [a, c]
-                        [x] -> bound_pred a c x [] []
-                        (x::xs) -> bound_pred a c x [] xs
-                    [prev] -> bound_pred prev a c [] rest
-                    (prev::xs) -> bound_pred prev a c xs rest
-
--- compares the x-value / d0-value of two vertices
 compare_x : Vec2 -> Vec2 -> Order
 compare_x a b =
     compare (Math.Vector2.getX a) (Math.Vector2.getX b)
 
+constructChain : List Vec2 -> List Vec2
+constructChain vertices =
+    case vertices of
+       [] -> []
+       [a] -> [a]
+       [a,b] -> [a,b]
+       a::b::c::xs -> chainRecursive a b c [] xs
 
--- Determines whether the passed vertices form a right turn or not.
-rightTurnUpper : Vec2 -> Vec2 -> Vec2 -> Bool
-rightTurnUpper a b c =
+-- Recursively calculates a single boundary of a convex hull,
+-- ensuring any given 3 consequitive points form a right turn in accordnace with the passed predicate.
+chainRecursive : Vec2 -> Vec2 -> Vec2 -> List Vec2 -> List Vec2 -> List Vec2
+chainRecursive a b c acc rest =
+    if isRightTurn a b c then
+        case rest of
+            [] -> List.reverse acc ++ [a,b,c] -- used as stack so far, now reverse for desired order
+            [x] -> chainRecursive b c x (a :: acc) []
+            (x::xs) -> chainRecursive b c x (a :: acc) xs
+    else
+        case acc of
+            [] -> case rest of
+                [] -> [a, c]
+                [x] -> chainRecursive a c x [] []
+                (x::xs) -> chainRecursive a c x [] xs
+            [prev] -> chainRecursive prev a c [] rest
+            (prev::xs) -> chainRecursive prev a c xs rest
+
+isRightTurn : Vec2 -> Vec2 -> Vec2 -> Bool
+isRightTurn a b c =
+    case orientation a b c of
+       Clockwise -> True
+       _ -> False
+
+type Orientation = Clockwise | Counterclockwise | Colinear
+
+orientation : Vec2 -> Vec2 -> Vec2 -> Orientation
+orientation a b c =
     let
-        slope = Math.Vector2.sub b a
-        scale = (Math.Vector2.sub c a |> Math.Vector2.getX ) / Math.Vector2.getX(slope)
-        expected = Math.Vector2.add a ( Math.Vector2.scale scale slope)
+        slope_a_b = ( getY b - getY a ) / ( getX b - getX a)
+        slope_b_c = ( getY c - getY b ) / ( getX c - getX b)
     in
-        case compare (Math.Vector2.getY c) (Math.Vector2.getY expected) of
-            LT -> True
-            EQ -> False
-            GT -> False
-
-
--- Determines whether the passed vertices form a right turn or not.
-rightTurnLower : Vec2 -> Vec2 -> Vec2 -> Bool
-rightTurnLower a b c =
-    let
-        slope = Math.Vector2.sub b a
-        scale = (Math.Vector2.sub c a |> Math.Vector2.getX ) / Math.Vector2.getX(slope)
-        expected = Math.Vector2.add a ( Math.Vector2.scale scale slope)
-    in
-        case compare (Math.Vector2.getY c) (Math.Vector2.getY expected) of
-            LT -> False
-            EQ -> False
-            GT -> True
+        case compare slope_a_b slope_b_c of
+           LT -> Counterclockwise
+           GT -> Clockwise
+           EQ -> Colinear
